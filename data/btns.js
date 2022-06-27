8e58a8bc5acc1ca70b1245c7af32a1f3a562a07c
fuadhy <fuadmail3@gmail.com>
2022-06-27 15:16:01 +0300

    initial

---
+++ data/btns.js
@@ -0,0 +1,56 @@
+const btns = [
+    {
+      value: '7'
+    },
+    {
+      value: '8'
+    },
+    {
+      value: '9'
+    },
+    {
+      value: 'DEL',
+    },
+    {
+      value: '4'
+    },
+    {
+      value: '5'
+    },
+    {
+      value: '6'
+    },
+    {
+      value: '+',
+      operator: true
+    },
+    {
+      value: '1'
+    },
+    {
+      value: '2'
+    },
+    {
+      value: '3'
+    },
+    {
+      value: '-',
+      operator: true
+    },
+    {
+      value: '.'
+    },
+    {
+      value: '0'
+    },
+    {
+      value: '/',
+      operator: true
+    },
+    {
+      value: 'x',
+      operator: true
+    }
+]
+
+export default btns
